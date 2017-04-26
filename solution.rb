c566363f9cefa318a967da796e85fd5698c26db8
Juan David Cano <juancano5304@gmail.com>
2017-04-26 10:48:01 -0500

    agregar !

--- solution.rb
+++ solution.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 
 get '/:name' do 
-	unless params[:name]
-		"Hola Desconocido!"
-	else
-		"Hola #{params[:name]}"
-	end
+#	unless params[:name]
+#		"Hola Desconocido!"
+#	else
+		"Hola #{params[:name]}!"
+#	end
 end
