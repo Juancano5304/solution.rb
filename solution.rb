199277532c3a07fe18ae74e2207716bd4352704f
Juan David Cano <juancano5304@gmail.com>
2017-07-25 10:56:15 -0500

    reto 2

--- solution.rb
+++ solution.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
+=begin
 get '/' do 
 	unless params[:nombre]
 		"Hola desconocido!"
 	else
 		"Hola #{params[:nombre]}!"
 	end
+end
+=end
+
+get '/:nombre' do
+	"Hola #{params[:nombre].capitalize}!"
 end
